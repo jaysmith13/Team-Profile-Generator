2e9de7a679c877ea9de76f4caa63c77b84b02db3
jaysmith13 <janeasmith13@gmail.com>
2022-07-25 20:34:52 -0600

    Update generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,6 +5,12 @@ const generateManager = function(manager){
     <div class="col-4 mt-4">
         <div class ="card h-100">
         <div class="card-header">
-    
+        <h3>${manager.name}</h3>
+        <h4>Manager</h4><i class ="material-icons">content_paste</i>
+        </div>
+
+        <div class="card-body">
+            <p class="id">ID: ${manager.id}</p>
+            <p class
     `
 }
