97bac8f4890e63117ebfc6c9dec9914b212a7f52
jaysmith13 <janeasmith13@gmail.com>
2022-07-25 20:29:45 -0600

    src generate create

---
+++ src/generateHTML.js
@@ -0,0 +1,10 @@
+//manager card
+
+const generateManager = function(manager){
+    return`
+    <div class="col-4 mt-4">
+        <div class ="card h-100">
+        <div class="card-header">
+    
+    `
+}
